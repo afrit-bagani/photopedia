d16e78363dceba97f48da8c1f97ec57d87c5217d
afrit-bagani <o6228466@gmail.com>
2025-05-02 16:35:23 -0930

    Added Home

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { HiMenu } from "react-icons/hi";
+import { IoCloseSharp } from "react-icons/io5";
+
+import Sidebar from "../components/Sidebar";
+
+function Home() {
+  const user = {
+    _id: "wufh",
+    name: "Afrit Bagani",
+  };
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  return (
+    <>
+      {/* Desktop View */}
+      <div className="d-none d-md-flex">
+        <Sidebar user={user} />
+      </div>
+
+      {/* Mobile View */}
+      <div className="d-flex d-md-none w-50 ">
+        <HiMenu onClick={() => setToggleSidebar((prev) => !prev)} size={30} />
+        {toggleSidebar && (
+          <div className="bg-dark-subtle w-100">
+            <div className="mb-5">
+              <IoCloseSharp
+                onClick={() => setToggleSidebar(false)}
+                className="float-end"
+                size={25}
+              />
+            </div>
+            <Sidebar closeToogle={setToggleSidebar} user={user} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Home;
