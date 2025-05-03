0f4cb8729c282d0bc2d838f508d023b278923940
afrit-bagani <o6228466@gmail.com>
2025-05-03 07:37:48 +0530

    Added App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./layouts/Main";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Main />,
-      children: [
-        {
-          path: "/login",
-          element: <Login />,
-        },
-      ],
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "*",
+      element: <Error404 />,
     },
   ]);
 
