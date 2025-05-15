2be502e6454b12872181b4c8f88d0a953c76efca
afrit-bagani <o6228466@gmail.com>
2025-05-15 16:32:25 +0530

    Adde navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar({ searchTerm, setSearchTerm, user }) {
+  return (
+    <>
+      <div className="d-flex w-100 gap-2">
+        <div className="d-flex justify-content-start items-center w-100 rounded"></div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
